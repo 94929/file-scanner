4e8e010a57c9f4fdb5afc10efb5fb72cc7b759c7
jsung5381 <jsung5381@naver.com>
2016-11-20 21:53:33 +0900

    Added docx extension file for testing purpose

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,10 @@
 
 public class Main {
     public static void main(String[] args) {
+        // Obtaining the root path
         String path = System.getProperty("user.dir");
+
+        // The FileScanner will scan text files only
         FileScanner fs = new FileScanner("txt");
 
         // Takes an input of the 'root' path
